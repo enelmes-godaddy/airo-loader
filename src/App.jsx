66524d9e10a865ae855a8a9c2e6886ea97659566
Elena Nelmes <enelmes@godaddy.com>
2025-06-11 15:34:16 -0500

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import React, { useState } from 'react'
-import clsx from 'clsx'
-import './App.css'
+import React, { useState } from "react";
+import clsx from "clsx";
+import "./App.css";
 
-import Sparkle from './assets/sparkle.tsx'
-import Wand from './assets/wand.tsx'
+import Sparkle from "./assets/sparkle.tsx";
+import Wand from "./assets/wand.tsx";
 
-const Sparkle1 = Sparkle
-const Sparkle2 = Sparkle
-const Sparkle3 = Sparkle
+const Sparkle1 = Sparkle;
+const Sparkle2 = Sparkle;
+const Sparkle3 = Sparkle;
 
 function App() {
-  const [isPlaying, setIsPlaying] = useState(true)
+  const [isPlaying, setIsPlaying] = useState(true);
 
   return (
     <>
-      <div className={clsx('airo-loading-animation', {
-        'airo-loading-animation--playing': isPlaying,})}
+      <div
+        className={clsx("airo-loading-animation", {
+          "airo-loading-animation--playing": isPlaying,
+        })}
       >
-        <Sparkle1 className='sparkle sparkle1' />
-        <Sparkle2 className='sparkle sparkle2' />
-        <Sparkle3 className='sparkle sparkle3' />
-        <Wand className='wand' />
+        <Sparkle1 className="sparkle sparkle1" />
+        <Sparkle2 className="sparkle sparkle2" />
+        <Sparkle3 className="sparkle sparkle3" />
+        <Wand className="wand" />
       </div>
       <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? 'Stop' : 'Play'}
+        {isPlaying ? "Stop" : "Play"}
       </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
