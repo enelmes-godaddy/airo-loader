62fe1543d209e8fe754bc44448e099546968ef4a
Elena Nelmes <enelmes@godaddy.com>
2025-06-11 15:28:42 -0500

    start animation work

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import clsx from 'clsx'
 import './App.css'
 
+import Sparkle from './assets/sparkle.tsx'
+import Wand from './assets/wand.tsx'
+
+const Sparkle1 = Sparkle
+const Sparkle2 = Sparkle
+const Sparkle3 = Sparkle
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(true)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={clsx('airo-loading-animation', {
+        'airo-loading-animation--playing': isPlaying,})}
+      >
+        <Sparkle1 className='sparkle sparkle1' />
+        <Sparkle2 className='sparkle sparkle2' />
+        <Sparkle3 className='sparkle sparkle3' />
+        <Wand className='wand' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? 'Stop' : 'Play'}
+      </button>
     </>
   )
 }
